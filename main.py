76a8692e1908bf987ae99e363d4adfcd1e39c880
bei <zhibei66@gmail.com>
2025-04-08 21:03:05 +0800

    更新版本号至v1.3.4，完善模型名称映射功能，增加详细描述以支持友好名称到实际模型名称的映射。

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from astrbot.api.provider import LLMResponse
 PLUGIN_NAME = "astrbot_plugin_GPT_SoVITS"
 PLUGIN_AUTHOR = "Zhalslar"
 PLUGIN_DESCRIPTION = "GPT_SoVITS对接插件"
-PLUGIN_VERSION = "1.3.3"
+PLUGIN_VERSION = "1.3.4"
 
 # 目录配置
 SAVED_AUDIO_DIR = Path("./data/plugins_data/astrbot_plugin_GPT_SoVITS")
